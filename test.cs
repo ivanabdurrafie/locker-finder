fa4c629a24d62df8dd20f1b0d2c3eff13bfa40f2
ivanabdurrafie <iabdurrafie48@gmail.com>
2022-11-16 13:04:01 +0700

    add more test

--- test.cs
+++ test.cs
@@ -10,6 +10,10 @@ namespace MyNamespace
             Assert.Equal(1, Program.returnFloor(5));
             Assert.Equal(2, Program.returnFloor(11));
             Assert.Equal(3, Program.returnFloor(18));
+            Assert.Equal(4, Program.returnFloor(23));
+            Assert.Equal(5, Program.returnFloor(29));
+            Assert.Equal(6, Program.returnFloor(36));
+            
         }
     }
 }
